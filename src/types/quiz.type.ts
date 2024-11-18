5fccf3d9a3f9cc8b93e0c2dcedd3a22c4e17b229
Farid Gasimov <faridgasimov.93@gmail.com>
2024-11-18 19:43:40 +0400

    completed project

---
+++ src/types/quiz.type.ts
@@ -0,0 +1,17 @@
+export type QuizType = {
+  id: number,
+  name: string,
+  questions: Array<QuizQuestionType>
+}
+
+export type QuizQuestionType = {
+  id: number,
+  question: string,
+  answers: Array<QuizAnswerType>
+}
+
+export type QuizAnswerType = {
+  correct: any
+  id: number,
+  answer: string
+}
